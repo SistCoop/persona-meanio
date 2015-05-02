921e6e83fcda2dffd2e31cadc4b019c1c014ea45
carlos <carlosthe19916@gmail.com>
2015-05-02 14:00:05 -0500

    desactivar personas

--- public/controllers/natural/EditarPersonaNaturalCtrl.js
+++ public/controllers/natural/EditarPersonaNaturalCtrl.js
@@ -2,10 +2,16 @@
 
 /* jshint -W098 */
 angular.module('mean.persona').controller('EditarPersonaNaturalCtrl', function(
-    $scope, $state, personaNatural, SGPersonaNatural, toastr){
+    $scope, $state, personaNatural, SGDialog, toastr){
 
     $scope.view = {
         persona: personaNatural
+    };
+
+    $scope.desactivar = function(){
+        SGDialog.confirm('Desactivar', 'Estas seguro de querer desactivar el/la persona', function() {
+            toastr.info('Las personas no pueden ser desactivadas, solo actualizadas');
+        });
     };
 
 });
