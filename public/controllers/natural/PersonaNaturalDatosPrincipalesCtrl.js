856abfbdad433aa6cc54a5139e63fcf4cd1d0a97
carlos <carlosthe19916@gmail.com>
2015-05-02 10:13:41 -0500

    no se puede editar tipo y numero de documento

--- public/controllers/natural/PersonaNaturalDatosPrincipalesCtrl.js
+++ public/controllers/natural/PersonaNaturalDatosPrincipalesCtrl.js
@@ -2,7 +2,7 @@
 
 /* jshint -W098 */
 angular.module('mean.persona').controller('PersonaNaturalDatosPrincipalesCtrl', function(
-    $scope, $state, personaNatural, SGCountryCode, SGSexo, SGEstadoCivil, SGPersonaNatural, SGTipoDocumento, toastr){
+    $scope, $state, personaNatural, SGCountryCode, SGSexo, SGEstadoCivil, SGPersonaNatural, toastr){
 
     $scope.view = {
         persona: personaNatural
@@ -10,51 +10,26 @@ angular.module('mean.persona').controller('PersonaNaturalDatosPrincipalesCtrl', 
 
     $scope.combo = {
         pais: SGCountryCode.$search().$object,
-        tipoDocumento: SGTipoDocumento.$search({tipoPersona: 'natural'}).$object,
         sexo: SGSexo.$search().$object,
         estadoCivil: SGEstadoCivil.$search().$object
     };
     $scope.combo.selected = {
         pais: undefined,
-        tipoDocumento: undefined,
         sexo: undefined,
         estadoCivil: undefined
     };
 
-    $scope.check = function($event){
-        if(!angular.isUndefined($event))
-            $event.preventDefault();
-        if(!angular.isUndefined($scope.combo.selected.tipoDocumento) && !angular.isUndefined($scope.view.persona.numeroDocumento)){
-            SGPersonaNatural.$findByTipoNumeroDocumento($scope.combo.selected.tipoDocumento.abreviatura, $scope.view.persona.numeroDocumento).then(function(data){
-                if(!data)
-                    toastr.info('Documento de identidad disponible', 'Info');
-                else
-                    toastr.warning('Documento de identidad no disponible', 'Warning');
-            });
-        }
-    };
-
     $scope.submit = function(){
         if ($scope.form.$valid) {
-            var save = function(){
-                $scope.view.persona.$save().then(
-                    function(response){
-                        toastr.success('Persona actualizada', 'Success');
-                        $scope.view.personaDB = angular.copy($scope.view.persona);
-                    },
-                    function error(err){
-                        toastr.error(err.data.message, 'Error');
-                    }
-                );
-            };
-            SGPersonaNatural.$findByTipoNumeroDocumento($scope.view.persona.tipoDocumento, $scope.view.persona.numeroDocumento).then(function(data){
-                if(data && data.id !== $scope.view.persona.id){
-                    toastr.error('Documento de identidad no disponible', 'Error');
+            $scope.view.persona.$save().then(
+                function(response){
+                    toastr.success('Persona actualizada', 'Success');
+                    $scope.view.personaDB = angular.copy($scope.view.persona);
+                },
+                function error(err){
+                    toastr.error(err.data.message, 'Error');
                 }
-                else {
-                    save();
-                }
-            });
+            );
         }
     };
 
